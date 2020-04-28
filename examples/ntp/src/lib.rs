ba29d60405e38ed8c0b92ef9498337d8dfbf2c23
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-04-28 14:41:37 -0700

    define type signatures for Message Exchanges

--- examples/ntp/src/lib.rs
+++ examples/ntp/src/lib.rs
@@ -1,5 +1,19 @@
 use wasm_bindgen::prelude::*;
 
+/*
+   TODO:
+   1) define pub sub actor Message Exchange protocols
+     a) MessageExchange
+        * publish(descriptor_prefix) -> Outbox
+        * subscribe(selector, Inbox)
+     b) Outbox
+        * push(descriptor_suffix, json message)
+     c) Inbox
+        * new(selector, callback) -> Inbox
+   2) define setInterval as a message exchange
+   3) derive example to demonstrate clock skew and correction
+*/
+
 #[wasm_bindgen]
 extern "C" {
     fn setInterval(closure: &Closure<FnMut()>, time: u32) -> i32;
