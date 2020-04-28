c3146f5bcfdd2433e5c4830a950ef01856e4f83a
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-04-28 13:38:19 -0700

    threading isnt supported for wasm yet

--- examples/ntp/src/lib.rs
+++ examples/ntp/src/lib.rs
@@ -1,27 +1,24 @@
 use wasm_bindgen::prelude::*;
-use web_sys::console;
 
+fn parse_time(t: u64) -> (u64,u64,u64) {
+    (t/3600, (t/60)%60, t%60)
+}
 
-// When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
-// allocator.
-//
-// If you don't want to use `wee_alloc`, you can safely delete this.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-
-// This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
-    // This provides better error messages in debug mode.
-    // It's disabled in release mode so it doesn't bloat up the file size.
-    #[cfg(debug_assertions)]
-    console_error_panic_hook::set_once();
 
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
 
-    // Your code goes here!
-    console::log_1(&JsValue::from_str("Hello world!"));
+    //wasm has no threads
+    let i = 99999;
+    let (fh,fm,fs) = parse_time(i);
+    let (mh,mm,ms) = parse_time(i/2);
+    let (sh,sm,ss) = parse_time(i/4);
+    body.set_inner_html(
+       &format!("<p>{:02}:{:02}:{:02}</p><p>{:02}:{:02}:{:02}</p><p>{:02}:{:02}:{:02}</p>", fh,fm,fs, mh,mm,ms, sh,sm,ss)
+    );
 
     Ok(())
 }
