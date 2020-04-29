f5264809763a9c2e0a29945328f6c9c837094c68
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-04-29 14:28:26 -0700

    dont be so opinionated

--- examples/ntp_ftfy/src/lib.rs
+++ examples/ntp_ftfy/src/lib.rs
@@ -2,22 +2,32 @@ use wasm_bindgen::prelude::*;
 use std::sync::Mutex;
 use serde_json::{Value};
 
+fn parse_time(t: u64) -> (u64,u64,u64) {
+    (t/3600, (t/60)%60, t%60)
+}
+
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
     let start_time = 9*3600 + 10*60 + 11;
 
     let mut fast_timer: Mutex<u64> = Mutex::new(start_time);
-    HtmlActor::new("#fast_timer", "{:02}:{:02}:{:02}", vec![
+    HtmlActor::new("#fast_timer", |actor| {
+          let mut timer = fast_timer.lock().unwrap();
+          let (h,m,s) = parse_time(*timer);
+          format!("{:02}:{:02}:{:02}", h, m, s)
+       }, vec![
        ("fast_timer", "tick", move |msg| {
           let mut timer = fast_timer.lock().unwrap();
           *timer += 1;
+          true
        }),
        ("ntp", "set", move |msg| {
           if let Value::Number(time) = msg {
              let mut timer = fast_timer.lock().unwrap();
              *timer = time.as_u64().expect("ntp expected u64 time value");
-          }
-       }),
+          };
+          true
+       })],
     ]);
 
     setIntervalForget(|| { jsmx_exchange.push("fast_timer","tick",Value::Null) }, 900);
