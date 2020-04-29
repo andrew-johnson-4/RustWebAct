de4e61a2e5941052ce5161ea2be6cfb4ac2b0edd
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-04-29 11:56:05 -0700

    document methods are available already

--- examples/ntp/src/lib.rs
+++ examples/ntp/src/lib.rs
@@ -1,39 +1,5 @@
 use wasm_bindgen::prelude::*;
 
-/*
-   TODO:
-   1) define pub sub actor Message Exchange protocols
-     a) MessageExchange
-        * publish(descriptor_prefix) -> Outbox
-        * subscribe(selector, Inbox)
-     b) Outbox
-        * push(descriptor_suffix, json message)
-     c) Inbox
-        * new(selector, callback) -> Inbox
-   2) define setInterval as a message exchange
-   3) derive example to demonstrate clock skew and correction
-*/
-
-#[wasm_bindgen]
-extern "C" {
-    fn setInterval(closure: &Closure<FnMut()>, time: u32) -> i32;
-    fn clearInterval(id: i32);
-
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(s: &str);
-}
-
-#[wasm_bindgen]
-pub struct IntervalHandle {
-    interval_id: i32,
-    _closure: Closure<FnMut()>,
-}
-impl Drop for IntervalHandle {
-    fn drop(&mut self) {
-        clearInterval(self.interval_id);
-    }
-}
-
 fn parse_time(t: u64) -> (u64,u64,u64) {
     (t/3600, (t/60)%60, t%60)
 }
@@ -41,32 +7,21 @@ fn parse_time(t: u64) -> (u64,u64,u64) {
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
 
-    let cb = Closure::wrap(Box::new(|| {
-        log("interval elapsed!");
-    }) as Box<FnMut()>);
-    let interval_id = setInterval(&cb, 1_000);
+let window = web_sys::window().expect("should have a window in this context");
+    let document = window.document().expect("window should have a document");
 
-    let ih = IntervalHandle {
-        interval_id,
-        _closure: cb,
-    };
-
-    /*
-    let window = web_sys::window().expect("no global `window` exists");
-    let document = window.document().expect("should have a document on window");
-    let body = document.body().expect("document should have a body");
-
-    let mut i = 99999;
-    Interval::new(1000, || {
-       let (fh,fm,fs) = parse_time(i);
-       let (mh,mm,ms) = parse_time(i/2);
-       let (sh,sm,ss) = parse_time(i/4);
-       body.set_inner_html(
-          &format!("<p>{:02}:{:02}:{:02}</p><p>{:02}:{:02}:{:02}</p><p>{:02}:{:02}:{:02}</p>", fh,fm,fs, mh,mm,ms, sh,sm,ss)
-       );
-       i += 1;
-    })
-    */
+    document
+        .get_element_by_id("fast_timer")
+        .expect("should have #fast_timer on the page")
+        .set_inner_html(&format!("{:02}:{:02}:{:02}", 9, 14, 11));
+    document
+        .get_element_by_id("mid_timer")
+        .expect("should have #mid_timer on the page")
+        .set_inner_html(&format!("{:02}:{:02}:{:02}", 8, 14, 11));
+    document
+        .get_element_by_id("slow_timer")
+        .expect("should have #slow_timer on the page")
+        .set_inner_html(&format!("{:02}:{:02}:{:02}", 7, 14, 11));
 
     Ok(())
 }
