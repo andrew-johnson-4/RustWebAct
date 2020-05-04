31c115079b1dbcfa2784aa3d0a10940e53eb8f12
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-05-04 15:58:39 -0700

    idiomatic example is working

--- examples/ntp_ftfy/src/lib.rs
+++ examples/ntp_ftfy/src/lib.rs
@@ -2,8 +2,6 @@ use wasm_bindgen::prelude::*;
 use jsmx::{JSMX_EXCHANGE};
 use rustwebact::rwa_time::{set_interval_forget};
 use rustwebact::rwa_html::{HtmlActor};
-use std::sync::Mutex;
-use std::sync::Arc;
 use serde_json::{Value, Number};
 
 fn parse_time(t: u64) -> (u64,u64,u64) {
@@ -18,75 +16,49 @@ pub fn main_js() -> Result<(), JsValue> {
           let (h,m,s) = parse_time(*time);
           format!("{:02}:{:02}:{:02}", h, m, s)
        }, vec![
-       ("fast_timer", "tick", Box::new(|time, msg| {
+       ("fast_timer", "tick", Box::new(|time, _msg| {
           *time += 1;
           true
        })),
-       ("ntp", "set", Box::new(|&mut time, msg| {
-          /* if let Value::Number(n) = msg {
-             time = n.as_u64().unwrap();
-          }; */
+       ("ntp", "set", Box::new(|time, msg| {
+          if let Value::Number(n) = msg {
+             *time = n.as_u64().unwrap();
+          };
           true
        }))],
     );
 
-    let mid_seconds: Arc<Mutex<u64>> = Arc::new(Mutex::new(start_time));
-    let timer = mid_seconds.clone();
-    JSMX_EXCHANGE.listen("mid_timer", "tick", move |_msg| {
-       let mut mid_seconds = timer.lock().unwrap();
-       *mid_seconds += 1;
-       let (h,m,s) = parse_time(*mid_seconds);
-       let window = web_sys::window().expect("should have a window in this context");
-       let document = window.document().expect("window should have a document");
-       document
-           .get_element_by_id("mid_timer")
-           .expect("should have #mid_timer on the page")
-           .set_inner_html(&format!("{:02}:{:02}:{:02}", h, m, s));
-    });
-    let timer = mid_seconds.clone();
-    JSMX_EXCHANGE.listen("ntp", "set", move |msg| {
-       if let Value::Number(n) = msg {
-          let n = n.as_u64().unwrap();
-          let mut mid_seconds = timer.lock().unwrap();
-          *mid_seconds = n;
-          let (h,m,s) = parse_time(n);
-          let window = web_sys::window().expect("should have a window in this context");
-          let document = window.document().expect("window should have a document");
-          document
-             .get_element_by_id("mid_timer")
-             .expect("should have #mid_timer on the page")
-             .set_inner_html(&format!("{:02}:{:02}:{:02}", h, m, s));
-       }
-    });
+    HtmlActor::new("mid_timer", start_time, |time| {
+          let (h,m,s) = parse_time(*time);
+          format!("{:02}:{:02}:{:02}", h, m, s)
+       }, vec![
+       ("mid_timer", "tick", Box::new(|time, _msg| {
+          *time += 1;
+          true
+       })),
+       ("ntp", "set", Box::new(|time, msg| {
+          if let Value::Number(n) = msg {
+             *time = n.as_u64().unwrap();
+          };
+          true
+       }))],
+    );
 
-    let slow_seconds: Arc<Mutex<u64>> = Arc::new(Mutex::new(start_time));
-    let timer = slow_seconds.clone();
-    JSMX_EXCHANGE.listen("slow_timer", "tick", move |_msg| {
-       let mut slow_seconds = timer.lock().unwrap();
-       *slow_seconds += 1;
-       let (h,m,s) = parse_time(*slow_seconds);
-       let window = web_sys::window().expect("should have a window in this context");
-       let document = window.document().expect("window should have a document");
-       document
-           .get_element_by_id("slow_timer")
-           .expect("should have #slow_timer on the page")
-           .set_inner_html(&format!("{:02}:{:02}:{:02}", h, m, s));
-    });
-    let timer = slow_seconds.clone();
-    JSMX_EXCHANGE.listen("ntp", "set", move |msg| {
-       if let Value::Number(n) = msg {
-          let n = n.as_u64().unwrap();
-          let mut slow_seconds = timer.lock().unwrap();
-          *slow_seconds = n;
-          let (h,m,s) = parse_time(n);
-          let window = web_sys::window().expect("should have a window in this context");
-          let document = window.document().expect("window should have a document");
-          document
-             .get_element_by_id("slow_timer")
-             .expect("should have #slow_timer on the page")
-             .set_inner_html(&format!("{:02}:{:02}:{:02}", h, m, s));
-       }
-    });
+    HtmlActor::new("slow_timer", start_time, |time| {
+          let (h,m,s) = parse_time(*time);
+          format!("{:02}:{:02}:{:02}", h, m, s)
+       }, vec![
+       ("slow_timer", "tick", Box::new(|time, _msg| {
+          *time += 1;
+          true
+       })),
+       ("ntp", "set", Box::new(|time, msg| {
+          if let Value::Number(n) = msg {
+             *time = n.as_u64().unwrap();
+          };
+          true
+       }))],
+    );
 
     set_interval_forget(|| { JSMX_EXCHANGE.push("fast_timer","tick",&Value::Null) }, 900);
     set_interval_forget(|| { JSMX_EXCHANGE.push("mid_timer","tick",&Value::Null) }, 1000);
