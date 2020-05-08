4251fc66a90eff2fd195f346645ec8dac126479a
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-05-07 20:20:27 -0700

    the builder pattern is alright

--- src/rwa_html.rs
+++ src/rwa_html.rs
@@ -2,9 +2,60 @@ use serde_json::{Value};
 use std::sync::{Arc,Mutex};
 use std::borrow::{Borrow,BorrowMut};
 use jsmx::{JSMX_EXCHANGE};
+use std::collections::HashMap;
+
+pub struct HtmlDom {
+   tag: String,
+   style: HashMap<String,String>
+}
+impl HtmlDom {
+   pub fn render(&self) -> String {
+      format!("<{}></{}>", self.tag, self.tag)
+   }
+   pub fn z<'a>(&'a mut self) -> &'a mut HtmlDom {
+      self.style.insert("z-index".to_string(),"auto".to_string());
+      self
+   }
+   pub fn z1<'a>(&'a mut self) -> &'a mut HtmlDom {
+      self.style.insert("z-index".to_string(),"1".to_string());
+      self
+   }
+   pub fn z2<'a>(&'a mut self) -> &'a mut HtmlDom {
+      self.style.insert("z-index".to_string(),"2".to_string());
+      self
+   }
+   pub fn z3<'a>(&'a mut self) -> &'a mut HtmlDom {
+      self.style.insert("z-index".to_string(),"3".to_string());
+      self
+   }
+   pub fn z4<'a>(&'a mut self) -> &'a mut HtmlDom {
+      self.style.insert("z-index".to_string(),"4".to_string());
+      self
+   }
+   pub fn z5<'a>(&'a mut self) -> &'a mut HtmlDom {
+      self.style.insert("z-index".to_string(),"5".to_string());
+      self
+   }
+   pub fn z6<'a>(&'a mut self) -> &'a mut HtmlDom {
+      self.style.insert("z-index".to_string(),"6".to_string());
+      self
+   }
+   pub fn z7<'a>(&'a mut self) -> &'a mut HtmlDom {
+      self.style.insert("z-index".to_string(),"7".to_string());
+      self
+   }
+   pub fn z8<'a>(&'a mut self) -> &'a mut HtmlDom {
+      self.style.insert("z-index".to_string(),"8".to_string());
+      self
+   }
+   pub fn z9<'a>(&'a mut self) -> &'a mut HtmlDom {
+      self.style.insert("z-index".to_string(),"9".to_string());
+      self
+   }
+}
 
 pub struct HtmlActor<T> {
-   st: Arc<Mutex<T>>
+   st: Arc<Mutex<T>>,
 }
 impl<T> HtmlActor<T>
    where T: 'static + Send + Sync {
